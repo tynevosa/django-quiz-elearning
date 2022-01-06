7f75718c33515fde642c84d87e2401ff949334f8
tynevosa <michael.edy623@gmail.com>
2022-01-07 00:59:23 +0200

    Add initial login/ register functionality

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
 
-app_name = 'test'
+app_name = 'quiz'
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('home/', views.home, name='home'),
+    path('register/', views.register, name='register'),
     path('', views.testmath, name='testmath'),
-
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
 def testmath(request):
     return render(request, 'testmath.html')
+
+# Views
+@login_required
+def home(request):
+    return render(request, "registration/success.html", {})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('quiz:home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
