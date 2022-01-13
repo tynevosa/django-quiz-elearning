c1640bf7a025079dfe5547a3514274c29ee2dfb0
tynevosa <michael.edy623@gmail.com>
2022-01-13 02:56:24 +0200

    Use prepopulated country/ city in registeration

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+
 from authentication.models import StudentProfile
 
 
 class StudentProfileForm(forms.ModelForm):
     birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD', widget=forms.DateInput(attrs={'type': 'date'}))
+    country = forms.CharField(widget=forms.Select(attrs={'id':'countryId','value':'Egypt', 'class':'countries'}))
+    city = forms.CharField(widget=forms.Select(attrs={'id':'stateId','class':'states'}))
 
     class Meta:
         model = StudentProfile
